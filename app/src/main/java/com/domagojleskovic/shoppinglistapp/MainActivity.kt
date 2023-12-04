21f0a78b13d99d5e84c81f956bc55fd59c05ac81
=domagojleskovic <domagoj.leskovic24@gmail.com>
2023-12-04 21:06:57 +0100

    Draw Add-item button in the middle of the screen

--- app/src/main/java/com/domagojleskovic/shoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/domagojleskovic/shoppinglistapp/MainActivity.kt
@@ -3,11 +3,15 @@ package com.domagojleskovic.shoppinglistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.domagojleskovic.shoppinglistapp.ui.theme.ShoppingListAppTheme
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DrawMenu()
                 }
             }
         }
@@ -30,17 +34,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DrawMenu(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Add item")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ShoppingListAppTheme {
-        Greeting("Android")
-    }
+fun DrawMenu_Preview(){
+    DrawMenu()
 }
