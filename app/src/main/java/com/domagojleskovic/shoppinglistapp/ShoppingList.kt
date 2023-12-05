2edfad7486e280e34c5cbf6d20510d94866506e8
=domagojleskovic <domagoj.leskovic24@gmail.com>
2023-12-05 20:12:57 +0100

    Insert image buttons for editing and deletion of items in the shopping app

--- app/src/main/java/com/domagojleskovic/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/domagojleskovic/shoppinglistapp/ShoppingList.kt
@@ -11,9 +11,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,6 +29,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.dp
 import com.domagojleskovic.shoppinglistapp.ui.theme.Purple40
 import com.domagojleskovic.shoppinglistapp.ui.theme.Purple80
@@ -137,5 +143,14 @@ fun ShoppingListItem(
             )
         ){
             Text(text = item.name, modifier = Modifier.padding(8.dp))
+            Text(text = "Qty: ${item.quantity}", modifier = Modifier.padding(8.dp))
+        Row(modifier = Modifier.padding(8.dp)){
+            IconButton(onClick = onEditClick) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+            }
+            IconButton(onClick = onDeleteClick) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+            }
+        }
     }
 }
